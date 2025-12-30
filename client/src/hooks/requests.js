60476708d6cb28e6d965665c9ccf30085332b403
firminkeambou <seven7digit2021@gmail.com>
2025-12-30 18:41:38 +0100

    adding continuous integration with node.yml file,therefore, workflow pipeline is configured

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,41 +1,37 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = 'http://localhost:8000/v1';
 async function httpGetPlanets() {
   // TODO: Once API is ready. // the following code is added because our API is now ready
- try {
-   const response = await fetch(`${API_URL}/planets`)
-  // Load planets and return as JSON.
-   return await response.json()
- } catch (error) {
-   console.error('Error fetching planets:', error)
-   
- }
- 
+  try {
+    const response = await fetch(`${API_URL}/planets`);
+    // Load planets and return as JSON.
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching planets:', error);
+  }
 }
 
 async function httpGetLaunches() {
   // TODO: Once API is ready.
   try {
-   const response = await fetch(`${API_URL}/launches`)
-   // Load launches, sort by flight number, and return as JSON.
-    const fetchedLaunches = await response.json()
-   return fetchedLaunches.sort((a, b) => {
-             return a.flightNumber - b.flightNumber
-         })
- } catch (error) {
-   console.error('Error fetching launches:', error)
-   
- }
- 
+    const response = await fetch(`${API_URL}/launches`);
+    // Load launches, sort by flight number, and return as JSON.
+    const fetchedLaunches = await response.json();
+    return fetchedLaunches.sort((a, b) => {
+      return a.flightNumber - b.flightNumber;
+    });
+  } catch (error) {
+    console.error('Error fetching launches:', error);
+  }
 }
 
 async function httpSubmitLaunch(launch) {
   // TODO: Once API is ready.
   // Submit given launch data to launch system.
-      try {
-      const response = await fetch(`${API_URL}/launches`, {
-      method: "POST",
+  try {
+    const response = await fetch(`${API_URL}/launches`, {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(launch),
     });
@@ -46,22 +42,22 @@ async function httpSubmitLaunch(launch) {
       message: 'Failed to submit launch',
     };
   }
- // Return the response as JSON.
+  // Return the response as JSON.
 }
 
 async function httpAbortLaunch(id) {
   // TODO: Once API is ready.
   // Delete launch with given ID.
-   try {
-      const response = await fetch(`${API_URL}/launches/${id}`, {
-      method: "DELETE",
+  try {
+    const response = await fetch(`${API_URL}/launches/${id}`, {
+      method: 'DELETE',
       headers: {
-        "Content-Type": "application/json",
-      }
+        'Content-Type': 'application/json',
+      },
     });
     return await response.json();
   } catch (err) {
-    console.log(err)
+    console.log(err);
     return {
       sucess: false,
       message: 'Failed to abort the launch',
@@ -69,9 +65,4 @@ async function httpAbortLaunch(id) {
   }
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
